5959f1659e548020d0b4cb4b85f81699d2c13a99
Roshith P <roshith844@gmail.com>
2022-10-20 15:26:07 +0530

    built layout and Rrmove 'public ' folder

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const expressLayouts = require('express-ejs-layouts')
 // server for localprot
 const server = http.createServer(app);
 server.listen(8080);
-// static files
-app.use(express.static('public'))
-app.use('/css',express.static(__dirname+'public'))
 // sets ejs view engine
 app.use(expressLayouts)
+app.set('layout', './layouts/page.ejs' )
 app.set('view engine', 'ejs');
 
 //navigation
 app.get('', (req,res) => {
      res.render('index')
+})
+app.get('/about',(req,res)=>{
+     res.render('about')
 })
 
 
